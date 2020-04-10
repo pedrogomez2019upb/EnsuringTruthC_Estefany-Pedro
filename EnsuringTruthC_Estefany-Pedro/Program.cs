e0d2b89da2dba96394981dd48acde932b67ae534
Pedro Felipe Gomez Bonilla <pedro.gomez.2019@upb.edu.co>
2020-04-09 19:28:49 -0500

    Initial Commit

---
+++ EnsuringTruthC_Estefany-Pedro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EnsuringTruthC_EstefanyPedro
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
